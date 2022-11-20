c6f99fc7147519c851bea9dbdb3a47c7aadaba30
ezio <eziopborges@gmail.com>
2022-11-20 13:50:17 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -14,14 +14,17 @@ function SalesCard() {
   const [minDate, setMinDate] = useState(min);
   const [maxDate, setMaxDate] = useState(max);
 
+  const dmin = minDate.toISOString().slice(0, 10);
+  const dmax = maxDate.toISOString().slice(0, 10);
+
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`)
+    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
       .then((response) => {
         setSales(response.data.content);
       })
-  }, [])
+  }, [minDate, maxDate])
 
   return (
     <div className="dsmeta-card">
@@ -76,37 +79,9 @@ function SalesCard() {
                 </tr>
               ))
             }
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="show992">#341</td>
-              <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
-              <td className="show992">15</td>
-              <td className="show992">11</td>
-              <td>R$ 55300.00</td>
-              <td>
-                <div className="dsmeta-red-btn-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
           </tbody>
-
         </table>
       </div>
-
     </div>
   );
 }
